cebb64fe80d14a9eec8519822a978cc3dfe43685
BigglePiggy <eddietbrooks@me.com>
2020-07-13 11:25:35 +0100

    Pylon Animation

--- TempleImperium/Assets/Scripts/Game Logic/GamePylon.cs
+++ TempleImperium/Assets/Scripts/Game Logic/GamePylon.cs
@@ -7,78 +7,35 @@ using UnityEngine;
 public class GamePylon : MonoBehaviour
 {
     ////Declarations
-    //Public
-    public float speed;
-    public bool startLowered;
-    public Vector3 raisedPosition;
-    public Vector3 loweredPosition;
-    
     //Private
-    private Transform core;
-    private bool up, down;
-
-    private float startTime;
-    private float journeyLength;
-
-    GameObject oGameLogic;  //gameLogic instance reference
+    private GameObject oGameLogic;
+    private Animator anim;
 
     //Initalization
     void Start()
     {
-        core = transform.Find("Core");
-
-        if (startLowered)
-        { core.localPosition = loweredPosition; }
-
-        else
-        { core.localPosition = raisedPosition; }
-
-        //establish reference to gamelogic script
         oGameLogic = GameObject.Find("Game Logic");
-    }
-
-    //Called per frame
-    void Update()
-    {
-        if (oGameLogic == null) //if no reference to gamelogic script
-        {
-        }
-
-        if ((up && core.localPosition != raisedPosition) || (down && core.localPosition != loweredPosition)) 
-        {
-            float distCovered = (Time.time - startTime) * speed;
-            float fractionOfJourney = distCovered / journeyLength;
-
-            if (up)
-            { core.localPosition = Vector3.Lerp(core.localPosition, raisedPosition, fractionOfJourney); }
-
-            if (down)
-            { core.localPosition = Vector3.Lerp(core.localPosition, loweredPosition, fractionOfJourney); }
-        }
+        anim = GetComponent<Animator>();
     }
 
     //Raise the pylon
-    public void GoUp() 
+    public void GoUp()
     {
-        startTime = Time.time;
-        journeyLength = Vector3.Distance(core.localPosition, raisedPosition);
-        up = true;
-        down = false;
+        anim.SetBool("Raise", true);
+        anim.SetBool("Lower", false);
     }
 
     //Lower the pylon
     public void GoDown()
     {
-        startTime = Time.time;
-        journeyLength = Vector3.Distance(core.localPosition, loweredPosition);
-        up = false;
-        down = true;
+        anim.SetBool("Raise", false);
+        anim.SetBool("Lower", true);
     }
 
     //Collision detection
     private void OnTriggerEnter(Collider ForeignCollider)
     {
-        if (up)
+        if (anim.GetBool("Raise"))
         {
             //if foreign collider is the player, this pylon goes down
             if (ForeignCollider.tag == "Player") { GoDown(); }
