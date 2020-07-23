50c833792f5d234ff51d4cea810de6486ad2863f
BigglePiggy <eddietbrooks@me.com>
2020-07-23 16:53:31 +0100

    Ed Changes + Jumppad

---
+++ TempleImperium/Assets/Scripts/JumpPad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    public float jumpForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player")) 
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce(0, jumpForce, 0);       
+        }
+    }
+}
