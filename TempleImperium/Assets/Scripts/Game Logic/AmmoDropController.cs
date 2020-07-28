19d371f7db4703624949eff2c1c167e7a74814f7
pontpontpont <62549042+pontpontpont@users.noreply.github.com>
2020-07-28 13:48:38 +0100

    AmmoDropController bits

---
+++ TempleImperium/Assets/Scripts/Game Logic/AmmoDropController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//written by Ase
+
+
+public class AmmoDropController : MonoBehaviour
+{
+    //this class manages ammo drop chances and spawning
+
+
+    public GameObject oAmmoBoxPrefab;
+    [Tooltip("Ammo boxes will reward one magazine by default.")]
+    public float m_fAmmoPerBoxMultiplierPrimary = 1.5f;
+    public float m_fAmmoPerBoxMultiplierSecondary = 1;
+
+    GameObject oPlayer; //player obj ref
+
+
+    void Start()
+    {
+        oPlayer = GameObject.Find("Player");    //establish reference to Player
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void RollDropChance(Vector3 input_spawnPosition)
+    {
+        //get ammo counts via Player tuple(item1 current reserve, max)
+        int m_iPrimaryCurrent;
+
+    }
+}
